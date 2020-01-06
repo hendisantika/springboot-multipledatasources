2fd060f40e974b2bea0fbeb0dc54702b72d7bcc5
Hendi Santika <hendisantika@gmail.com>
2020-01-07 06:39:44 +0700

    #14 - Add cardTransactionManager in CardDataSourceConfiguration

--- src/main/java/com/hendisantika/springbootmultipledatasources/config/CardDataSourceConfiguration.java
+++ src/main/java/com/hendisantika/springbootmultipledatasources/config/CardDataSourceConfiguration.java
@@ -2,13 +2,16 @@ package com.hendisantika.springbootmultipledatasources.config;
 
 import com.hendisantika.springbootmultipledatasources.model.card.Card;
 import org.apache.commons.dbcp.BasicDataSource;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
 
@@ -47,5 +50,11 @@ public class CardDataSourceConfiguration {
                 .dataSource(cardDataSource())
                 .packages(Card.class)
                 .build();
+    }
+
+    @Bean
+    public PlatformTransactionManager cardTransactionManager(
+            final @Qualifier("cardEntityManagerFactory") LocalContainerEntityManagerFactoryBean cardEntityManagerFactory) {
+        return new JpaTransactionManager(cardEntityManagerFactory.getObject());
     }
 }
