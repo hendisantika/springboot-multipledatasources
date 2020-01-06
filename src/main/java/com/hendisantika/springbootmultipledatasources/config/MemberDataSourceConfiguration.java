232fd8fd4c8e6ab519fef348febeaf1eaa2ee73d
Hendi Santika <hendisantika@gmail.com>
2020-01-07 06:42:22 +0700

    #22 - Add memberDataSource in MemberDataSourceConfiguration

--- src/main/java/com/hendisantika/springbootmultipledatasources/config/MemberDataSourceConfiguration.java
+++ src/main/java/com/hendisantika/springbootmultipledatasources/config/MemberDataSourceConfiguration.java
@@ -1,5 +1,6 @@
 package com.hendisantika.springbootmultipledatasources.config;
 
+import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -7,6 +8,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
 
 /**
  * Created by IntelliJ IDEA.
@@ -29,5 +32,13 @@ public class MemberDataSourceConfiguration {
     @ConfigurationProperties("app.datasource.member")
     public DataSourceProperties memberDataSourceProperties() {
         return new DataSourceProperties();
+    }
+
+    @Bean
+    @Primary
+    @ConfigurationProperties("app.datasource.member.configuration")
+    public DataSource memberDataSource() {
+        return memberDataSourceProperties().initializeDataSourceBuilder()
+                .type(HikariDataSource.class).build();
     }
 }
