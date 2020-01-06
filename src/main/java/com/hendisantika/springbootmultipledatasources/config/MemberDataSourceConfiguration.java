6697900b7853330b9f385588ff64a6d6dd2bd984
Hendi Santika <hendisantika@gmail.com>
2020-01-07 06:43:03 +0700

    #24 - Add memberTransactionManager in MemberDataSourceConfiguration

--- src/main/java/com/hendisantika/springbootmultipledatasources/config/MemberDataSourceConfiguration.java
+++ src/main/java/com/hendisantika/springbootmultipledatasources/config/MemberDataSourceConfiguration.java
@@ -2,6 +2,7 @@ package com.hendisantika.springbootmultipledatasources.config;
 
 import com.hendisantika.springbootmultipledatasources.model.member.Member;
 import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
@@ -9,7 +10,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
@@ -52,5 +55,12 @@ public class MemberDataSourceConfiguration {
                 .dataSource(memberDataSource())
                 .packages(Member.class)
                 .build();
+    }
+
+    @Primary
+    @Bean
+    public PlatformTransactionManager memberTransactionManager(
+            final @Qualifier("memberEntityManagerFactory") LocalContainerEntityManagerFactoryBean memberEntityManagerFactory) {
+        return new JpaTransactionManager(memberEntityManagerFactory.getObject());
     }
 }
