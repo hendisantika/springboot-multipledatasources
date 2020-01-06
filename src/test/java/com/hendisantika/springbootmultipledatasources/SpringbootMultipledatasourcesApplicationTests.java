aca079b137955fd76767d8f4db515cf313f61c90
Hendi Santika <hendisantika@gmail.com>
2020-01-07 06:48:15 +0700

    #26 - Set Before Testing

--- src/test/java/com/hendisantika/springbootmultipledatasources/SpringbootMultipledatasourcesApplicationTests.java
+++ src/test/java/com/hendisantika/springbootmultipledatasources/SpringbootMultipledatasourcesApplicationTests.java
@@ -6,9 +6,13 @@ import com.hendisantika.springbootmultipledatasources.model.member.Member;
 import com.hendisantika.springbootmultipledatasources.repository.card.CardRepository;
 import com.hendisantika.springbootmultipledatasources.repository.cardholder.CardHolderRepository;
 import com.hendisantika.springbootmultipledatasources.repository.member.MemberRepository;
+import org.junit.Before;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class SpringbootMultipledatasourcesApplicationTests {
 
@@ -29,5 +33,23 @@ class SpringbootMultipledatasourcesApplicationTests {
     private Member member;
     private Card card;
     private CardHolder cardHolder;
+
+    @Before
+    public void initializeDataObjects() {
+
+        member = new Member();
+        member.setMemberId("M001");
+        member.setName("Uzumaki Naruto");
+
+        cardHolder = new CardHolder();
+        cardHolder.setCardNumber("4111111111111111");
+        cardHolder.setMemberId(member.getMemberId());
+
+        card = new Card();
+        card.setExpirationMonth(01);
+        card.setExpirationYear(2020);
+        card.setName(member.getName());
+
+    }
 
 }
