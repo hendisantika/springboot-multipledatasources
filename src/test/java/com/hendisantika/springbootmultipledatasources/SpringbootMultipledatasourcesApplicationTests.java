929ceb0d27e8aae30c4e52d09645e9343ba8dfe0
Hendi Santika <hendisantika@gmail.com>
2020-01-07 06:25:32 +0700

    #1 - Initial Commit

---
+++ src/test/java/com/hendisantika/springbootmultipledatasources/SpringbootMultipledatasourcesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootmultipledatasources;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMultipledatasourcesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
