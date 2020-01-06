08cbca7b3d156599b8db5d5a411a27e4b7816acf
Hendi Santika <hendisantika@gmail.com>
2020-01-07 06:42:46 +0700

    #23 - Add memberEntityManagerFactory in MemberDataSourceConfiguration

--- src/main/java/com/hendisantika/springbootmultipledatasources/config/MemberDataSourceConfiguration.java
+++ src/main/java/com/hendisantika/springbootmultipledatasources/config/MemberDataSourceConfiguration.java
@@ -1,12 +1,15 @@
 package com.hendisantika.springbootmultipledatasources.config;
 
+import com.hendisantika.springbootmultipledatasources.model.member.Member;
 import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
@@ -40,5 +43,14 @@ public class MemberDataSourceConfiguration {
     public DataSource memberDataSource() {
         return memberDataSourceProperties().initializeDataSourceBuilder()
                 .type(HikariDataSource.class).build();
+    }
+
+    @Primary
+    @Bean(name = "memberEntityManagerFactory")
+    public LocalContainerEntityManagerFactoryBean memberEntityManagerFactory(EntityManagerFactoryBuilder builder) {
+        return builder
+                .dataSource(memberDataSource())
+                .packages(Member.class)
+                .build();
     }
 }
