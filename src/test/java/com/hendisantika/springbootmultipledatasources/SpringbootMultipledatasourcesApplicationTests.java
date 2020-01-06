d100cc88d06657453b58953ce4a36731d15148be
Hendi Santika <hendisantika@gmail.com>
2020-01-07 06:46:27 +0700

    #25 - Prepare for testing

--- src/test/java/com/hendisantika/springbootmultipledatasources/SpringbootMultipledatasourcesApplicationTests.java
+++ src/test/java/com/hendisantika/springbootmultipledatasources/SpringbootMultipledatasourcesApplicationTests.java
@@ -1,13 +1,33 @@
 package com.hendisantika.springbootmultipledatasources;
 
-import org.junit.jupiter.api.Test;
+import com.hendisantika.springbootmultipledatasources.model.card.Card;
+import com.hendisantika.springbootmultipledatasources.model.cardholder.CardHolder;
+import com.hendisantika.springbootmultipledatasources.model.member.Member;
+import com.hendisantika.springbootmultipledatasources.repository.card.CardRepository;
+import com.hendisantika.springbootmultipledatasources.repository.cardholder.CardHolderRepository;
+import com.hendisantika.springbootmultipledatasources.repository.member.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringbootMultipledatasourcesApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+    /*
+     * We will be using mysql databases we configured in our properties file for our tests
+     * Make sure your datasource connections are correct otherwise the test will fail
+     * */
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Autowired
+    private CardHolderRepository cardHolderRepository;
+
+    @Autowired
+    private CardRepository cardRepository;
+
+    private Member member;
+    private Card card;
+    private CardHolder cardHolder;
 
 }
