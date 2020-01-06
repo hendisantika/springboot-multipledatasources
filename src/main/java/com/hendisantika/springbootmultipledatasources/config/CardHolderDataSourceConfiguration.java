e7006b474dc981f539055b35c7ec539c97b19180
Hendi Santika <hendisantika@gmail.com>
2020-01-07 06:41:23 +0700

    #19 - Add cardHolderTransactionManager in CardHolderDataSourceConfiguration

--- src/main/java/com/hendisantika/springbootmultipledatasources/config/CardHolderDataSourceConfiguration.java
+++ src/main/java/com/hendisantika/springbootmultipledatasources/config/CardHolderDataSourceConfiguration.java
@@ -2,13 +2,16 @@ package com.hendisantika.springbootmultipledatasources.config;
 
 import com.hendisantika.springbootmultipledatasources.model.cardholder.CardHolder;
 import org.apache.commons.dbcp.BasicDataSource;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
 
@@ -46,6 +49,12 @@ public class CardHolderDataSourceConfiguration {
                 .dataSource(cardholderDataSource())
                 .packages(CardHolder.class)
                 .build();
+    }
+
+    @Bean
+    public PlatformTransactionManager cardHolderTransactionManager(
+            final @Qualifier("cardHolderEntityManagerFactory") LocalContainerEntityManagerFactoryBean cardHolderEntityManagerFactory) {
+        return new JpaTransactionManager(cardHolderEntityManagerFactory.getObject());
     }
 
 }
